2291e7c18be749feb792b0679614703295d261fa
oovissivoo <nurkanat02@inbox.ru>
2025-09-15 13:09:52 +0500

    server added

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func App() http.Handler {
+	router := http.NewServeMux()
+	router.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Hello world!")
+	})
+	return router
+}
 
 func main() {
-	fmt.Println("YTbot.yml")
+	app := App()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: app,
+	}
+	fmt.Println("YTbot listening")
+	server.ListenAndServe()
 }
